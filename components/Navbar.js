1551bb0d04c29f85144ca778b552faa1e697bfb4
Anjanay Raina <anjanayraina326@gmail.com>
2022-07-27 23:51:09 +0530

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+export const Navbar = () => {
+  return (
+    <div className="flex flex-row items-center  justify-between px-20 py-10">
+      <div className="flex flex-row items-center">
+        <h1 className="font-bold italic text-2xl text-white mr-10">Payments</h1>
+       </div>
+       </div>
+  );
+};
